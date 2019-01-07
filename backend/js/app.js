58b920bfbfbbdfa98f102c4527b7c69e9ea170c9
HarshadKapei <harshadkapei@gmail.com>
2019-01-07 12:35:09 +0530

    done

---
+++ backend/js/app.js
@@ -0,0 +1,151 @@
+// JavaScript Document
+var myApp = angular.module('myApp', [
+    'ui.router',
+    'pascalprecht.translate',
+    'angulartics',
+    'angulartics.google.analytics',
+    'imageupload',
+    "ngMap",
+    "internationalPhoneNumber",
+    'ui.bootstrap',
+    'ui.select',
+    'ngAnimate',
+    'toastr',
+    'textAngular',
+    'ngSanitize',
+    'ngMap',
+    'toggle-switch',
+    'cfp.hotkeys',
+    'ui.sortable'
+]);
+
+myApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+    // for http request with session
+    $httpProvider.defaults.withCredentials = true;
+    $stateProvider
+
+        .state('dashboard-main', {
+            url: "/dashboard-main",
+            templateUrl: "views/template.html",
+            controller: 'DashboardmainCtrl',
+        })
+
+        .state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "views/template.html",
+            controller: 'DashboardCtrl',
+        })
+
+        .state('enquiries', {
+            url: "/enquiries",
+            templateUrl: "views/template.html",
+            controller: 'EnquiriesCtrl',
+        })
+
+        .state('sources', {
+            url: "/sources",
+            templateUrl: "views/template.html",
+            controller: 'SourcesCtrl',
+        })
+
+        .state('customers', {
+            url: "/customers",
+            templateUrl: "views/template.html",
+            controller: 'CustomersCtrl',
+        })
+        .state('report', {
+            url: "/report",
+            templateUrl: "views/template.html",
+            controller: 'ReportCtrl',
+        })
+        .state('profile', {
+            url: "/profile",
+            templateUrl: "views/template.html",
+            controller: 'ProfileCtrl',
+        })
+        .state('enquiryinside', {
+            url: "/enquiryinside",
+            templateUrl: "views/template.html",
+            controller: 'EnquiryinsideCtrl',
+        })
+        .state('login', {
+            url: "/login",
+            templateUrl: "views/login.html",
+            controller: 'LoginCtrl'
+        })
+
+        .state('page', {
+            url: "/page/:id/{page:.*}/{keyword:.*}",
+            templateUrl: "views/template.html",
+            controller: 'PageJsonCtrl'
+        })
+
+        .state('loginapp', {
+            url: "/login/:id",
+            templateUrl: "views/login.html",
+            controller: 'LoginCtrl'
+        })
+
+        .state('country-list', {
+            url: "/country-list/{page:.*}/{keyword:.*}",
+            templateUrl: "views/template.html",
+            controller: 'CountryCtrl',
+            params: {
+                page: "1",
+                keyword: ""
+            }
+        })
+
+        .state('createcountry', {
+            url: "/country-create",
+            templateUrl: "views/template.html",
+            controller: 'CreateCountryCtrl'
+        })
+
+        .state('editcountry', {
+            url: "/country-edit/:id",
+            templateUrl: "views/template.html",
+            controller: 'EditCountryCtrl'
+        })
+
+        .state('schema-creator', {
+            url: "/schema-creator",
+            templateUrl: "views/template.html",
+            controller: 'SchemaCreatorCtrl'
+        })
+
+        .state('excel-upload', {
+            url: "/excel-upload/:model",
+            templateUrl: "views/template.html",
+            controller: 'ExcelUploadCtrl'
+        })
+        .state('add-admin', {
+            url: "/add-admin",
+            templateUrl: "views/template.html",
+            controller: 'AddadminCtrl'
+        })
+        .state('jagz', {
+            url: "/jagz",
+            templateUrl: "views/jagz.html",
+            controller: 'JagzCtrl'
+        })
+        .state('interview-schedule', {
+            url: "/interview-schedule",
+            templateUrl: "views/template.html",
+            controller: 'InterviewscheduleCtrl'
+        })
+        .state('interview-type', {
+            url: "/interview-type",
+            templateUrl: "views/template.html",
+            controller: 'InterviewTypeCtrl'
+        });
+
+    $urlRouterProvider.otherwise("/dashboard");
+    $locationProvider.html5Mode(isproduction);
+});
+
+myApp.config(function ($translateProvider) {
+    $translateProvider.translations('en', LanguageEnglish);
+    $translateProvider.translations('hi', LanguageHindi);
+    $translateProvider.preferredLanguage('en');
+});
---
+++ backend/js/factories/navigation.js
@@ -0,0 +1,181 @@
+var imgurl = adminurl + "upload/";
+
+var imgpath = imgurl + "readFile";
+var uploadurl = imgurl;
+
+myApp.factory("NavigationService", function($http) {
+  var navigation = [
+    {
+      name: "Dashboard",
+      classis: "active",
+      uisref: "dashboard"
+      // icon: "phone"
+    },
+    {
+      name: "Admin Master",
+      classis: "active",
+      uisref: "add-admin"
+      // icon: "phone"
+    },
+    {
+      name: "Enquiries",
+      classis: "active",
+      uisref: "enquiries"
+      // icon: "phone"
+    },
+    {
+      name: "Customers",
+      classis: "active",
+      uisref: "customers"
+      // icon: "phone"
+    },
+    {
+      name: "Sources",
+      classis: "active",
+      uisref: "sources"
+      // icon: "phone"
+    },
+    {
+      name: "Report",
+      classis: "active",
+      uisref: "report"
+      // icon: "phone"
+    },
+    {
+      name: "Profile DB",
+      classis: "active",
+      uisref: "profile"
+      // icon: "phone"
+    },
+    {
+      name: "Interview Schedule",
+      classis: "active",
+      uisref: "interview-schedule"
+      // icon: "phone"
+    },
+    {
+      name: "Type of Interview",
+      classis: "active",
+      uisref: "interview-type"
+      // icon: "phone"
+    },
+    {
+      name: "Product",
+      classis: "active",
+      sref: "#!/page/viewProduct//"
+      // icon: "phone"
+    },
+    {
+      name: "Brand",
+      classis: "active",
+      sref: "#!/page/viewBrand//"
+      // icon: "phone"
+    }
+  ];
+
+  return {
+    getnav: function() {
+      return navigation;
+    },
+
+    parseAccessToken: function(data, callback) {
+      if (data) {
+        $.jStorage.set("accessToken", data);
+        callback();
+      }
+    },
+    removeAccessToken: function(data, callback) {
+      $.jStorage.flush();
+    },
+    profile: function(callback, errorCallback) {
+      var data = {
+        accessToken: $.jStorage.get("accessToken")
+      };
+      $http.post(adminurl + "user/profile", data).then(function(data) {
+        data = data.data;
+        if (data.value === true) {
+          $.jStorage.set("profile", data.data);
+          callback();
+        } else {
+          errorCallback(data.error);
+        }
+      });
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+
+    search: function(url, formData, i, callback) {
+      $http.post(adminurl + url, formData).then(function(data) {
+        data = data.data;
+        callback(data, i);
+      });
+    },
+    delete: function(url, formData, callback) {
+      $http.post(adminurl + url, formData).then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    },
+    countrySave: function(formData, callback) {
+      $http.post(adminurl + "country/save", formData).then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    },
+
+    apiCall: function(url, formData, callback) {
+      $http.post(adminurl + url, formData).then(function(data) {
+        data = data.data;
+        callback(data);
+      });
+    },
+    searchCall: function(url, formData, i, callback) {
+      $http.post(adminurl + url, formData).then(function(data) {
+        data = data.data;
+        callback(data, i);
+      });
+    },
+
+    getOneCountry: function(id, callback) {
+      $http
+        .post(adminurl + "country/getOne", {
+          _id: id
+        })
+        .then(function(data) {
+          data = data.data;
+          callback(data);
+        });
+    },
+    getLatLng: function(address, i, callback) {
+      $http({
+        url:
+          "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+          address +
+          "&key=AIzaSyC62zlixVsjaq4zDaL4cefNCubjCgxkte4",
+        method: "GET",
+        withCredentials: false
+      }).then(function(data) {
+        data = data.data;
+        callback(data, i);
+      });
+    },
+    uploadExcel: function(form, callback) {
+      $http
+        .post(adminurl + form.model + "/import", {
+          file: form.file
+        })
+        .then(function(data) {
+          data = data.data;
+          callback(data);
+        });
+    }
+  };
+});
